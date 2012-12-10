1a2bad5a6fb6f88411c914d7a90415c079828271
Alex Chesser <achesser@klick.com>
2012-12-09 23:15:37 -0500

    Revert "cleanup unused view"
    This reverts commit 0edacc5e925e52e1ee73aeb3cd246c2beb693379.

---
+++ Nutrition Tracker/NTSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  NTSecondViewController.h
+//  Nutrition Tracker
+//
+//  Created by Application Developer on 12-12-07.
+//  Copyright (c) 2012 Alex Chesser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTSecondViewController : UIViewController
+
+@end
